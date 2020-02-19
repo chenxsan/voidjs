cbde1a3f158527e9516d8a3b8f10cd89bfdb485d
chenxsan <chenxsan@gmail.com>
2020-02-19 21:42:43 +0800

    Fix bug

--- static/js/index.js
+++ static/js/index.js
@@ -1,15 +1,16 @@
 /**
  * we write client side javascript here
- * you can write typescript too
  */
-document.querySelector('#cover').addEventListener('click', function(e) {
-  import('./log').then(({ default: log }) => {
-    log('you just click the cat!')
+const cover = document.querySelector('#cover')
+cover &&
+  cover.addEventListener('click', function(e) {
+    import('./log').then(({ default: log }) => {
+      log('you just click the cat!')
+    })
+    const classes = ['transform', 'scale-75', 'lg:scale-50']
+    if (e.target.classList.contains('transform')) {
+      e.target.classList.remove(...classes)
+    } else {
+      e.target.classList.add(...classes)
+    }
   })
-  const classes = ['transform', 'scale-75', 'lg:scale-50']
-  if (e.target.classList.contains('transform')) {
-    e.target.classList.remove(...classes)
-  } else {
-    e.target.classList.add(...classes)
-  }
-})
