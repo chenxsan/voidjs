830d22180bd650de2a3ed57826a107bcf3b4e0e7
chenxsan <chenxsan@gmail.com>
2020-02-19 09:07:22 +0800

    Add dynamic import

--- static/js/index.js
+++ static/js/index.js
@@ -2,9 +2,10 @@
  * we write client side javascript here
  * you can write typescript too
  */
-import log from './log'
 document.querySelector('#cover').addEventListener('click', function(e) {
-  log('you just click the cat!')
+  import('./log').then(({ default: log }) => {
+    log('you just click the cat!')
+  })
   const classes = ['transform', 'scale-75', 'lg:scale-50']
   if (e.target.classList.contains('transform')) {
     e.target.classList.remove(...classes)
