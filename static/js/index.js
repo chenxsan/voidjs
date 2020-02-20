c3c34f27b6899132ca468e0749ddb3a5bce5b210
chenxsan <chenxsan@gmail.com>
2020-02-20 21:47:17 +0800

    Use optional chain

--- static/js/index.js
+++ static/js/index.js
@@ -1,16 +1,14 @@
 /**
  * we write client side javascript here
  */
-const cover = document.querySelector('#cover')
-cover &&
-  cover.addEventListener('click', function(e) {
-    import('./log').then(({ default: log }) => {
-      log('you just click the cat!')
-    })
-    const classes = ['scale-75', 'lg:scale-50']
-    if (e.target.classList.contains('scale-75')) {
-      e.target.classList.remove(...classes)
-    } else {
-      e.target.classList.add(...classes)
-    }
+document.querySelector('#cover')?.addEventListener('click', function(e) {
+  import('./log').then(({ default: log }) => {
+    log('you just click the cat!')
   })
+  const classes = ['scale-75', 'lg:scale-50']
+  if (e.target.classList.contains('scale-75')) {
+    e.target.classList.remove(...classes)
+  } else {
+    e.target.classList.add(...classes)
+  }
+})
