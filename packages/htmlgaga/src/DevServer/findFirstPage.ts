b27a653fbf2c75e2d2700bad40082c5b638dae81
chenxsan <chenxsan@gmail.com>
2020-03-08 13:10:35 +0800

    Fix bug

--- packages/htmlgaga/src/DevServer/findFirstPage.ts
+++ packages/htmlgaga/src/DevServer/findFirstPage.ts
@@ -9,11 +9,13 @@ function isIndex(page: string): boolean {
   return getName(page) === 'index'
 }
 
+function sort(pathA: string, pathB: string): number {
+  return pathA.split(path.sep).length - pathB.split(path.sep).length
+}
+
 export default function findFirstPage(pages: string[]): string {
-  const indexPages = pages.filter(isIndex).sort((a, b) => a.length - b.length)
-  const nonIndexPages = pages
-    .filter(page => !isIndex(page))
-    .sort((a, b) => a.length - b.length)
+  const indexPages = pages.filter(isIndex).sort(sort)
+  const nonIndexPages = pages.filter(page => !isIndex(page)).sort(sort)
 
   return indexPages.length > 0 ? indexPages[0] : nonIndexPages[0]
 }
