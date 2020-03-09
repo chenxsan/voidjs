75797e7827b104a04a1df39c905f605153270f8f
chenxsan <chenxsan@gmail.com>
2020-03-09 23:14:20 +0800

    Fix bug

--- packages/htmlgaga/src/Builder.ts
+++ packages/htmlgaga/src/Builder.ts
@@ -153,7 +153,7 @@ class Builder {
             ]
           },
           {
-            test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+            test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
             use: [
               {
                 loader: 'file-loader',
--- packages/htmlgaga/src/DevServer/index.ts
+++ packages/htmlgaga/src/DevServer/index.ts
@@ -111,7 +111,7 @@ class DevServer {
             use: ['file-loader']
           },
           {
-            test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+            test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
             use: [
               {
                 loader: 'file-loader',
