1ffa1920561dd5b7e59c2d4320abf90864c3b99f
chenxsan <chenxsan@gmail.com>
2020-03-12 20:47:13 +0800

    Update jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,7 @@ module.exports = {
   rootDir: '.',
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testMatch: ['<rootDir>/packages/htmlgaga/src/**/*.test.ts'],
+  testMatch: ['<rootDir>/packages/*/src/**/*.test.ts'],
   globals: {
     'ts-jest': {
       packageJson: 'package.json'
