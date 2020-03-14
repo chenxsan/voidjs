bde14954ee6e13225034ebc9bc955a8d6a15525f
chenxsan <chenxsan@gmail.com>
2020-03-14 10:46:12 +0800

    Update doc

---
+++ packages/doc/components/Footer.js
@@ -0,0 +1,17 @@
+export default function Footer() {
+  return (
+    <footer className="text-center mt-32 text-gray-900">
+      Built with <strong className="font-bold text-black">htmlgaga</strong>
+      <div>
+        <a
+          target="_blank"
+          href="https://github.com/chenxsan/htmlgaga/tree/master/packages/doc"
+          rel="noopener noreferrer"
+          className="underline text-blue-600 hover:text-blue-800 transition duration-200"
+        >
+          Source code
+        </a>
+      </div>
+    </footer>
+  )
+}
--- packages/doc/pages/index.js
+++ packages/doc/pages/index.js
@@ -1,46 +1,32 @@
 import 'css/tailwind.css'
-
+import Pre from '../components/Pre'
+import P from '../components/P'
+import Footer from '../components/Footer'
 export default function Home() {
   return (
-    <div className="">
-      <h1 className="flex text-3xl leading-none justify-center pt-4 lg:text-4xl font-bold">
+    <div className="container mx-auto">
+      <h1 className="flex text-4xl leading-none justify-center pt-4 lg:text-4xl font-bold">
         htmlgaga
       </h1>
       <h2 className="flex justify-center text-base pt-2 px-2 leading-tight font-normal">
         Manage multiple non-SPA pages with webpack and React.js
       </h2>
-      <section className="pt-4 px-2">
-        <h2 className="font-medium text-xl">Get started</h2>
-        <ol className="list-decimal list-inside">
-          <li>
-            Install <code>htmlgaga</code>:
-            <pre>$ npm install htmlgaga --save-dev</pre>
-          </li>
-          <li>
-            Add npm scripts to <code>package.json</code>:
-            <pre>
-              <code>
-                {`
-              {
-                scripts: {
-                  dev: "htmlgaga dev",
-                  build: "htmlgaga build"
-                }
-              }
-              `}
-              </code>
-            </pre>
-          </li>
-          <li>
-            Create <code>index.js</code> file under <code>pages</code> directory
-            and add code{' '}
-            <code>{`import React from 'react'; export default function IndexPage() {return <div>hello htmlgaga</div>}`}</code>
-          </li>
-          <li>
-            Run <code>npm run dev</code> to start development server.
-          </li>
-        </ol>
+      <section className="pt-8 px-0 sm:px-2">
+        <h2 className="font-medium text-2xl">Get started</h2>
+        <P>To create a new htmlgaga project, run command:</P>
+        <Pre>
+          <code>npm init htmlgaga</code>
+        </Pre>
+        <P>If you are using yarn 1.x, run this instead:</P>
+        <Pre>
+          <code>yarn create htmlgaga</code>
+        </Pre>
+        <P>If you are using yarn 2.x, run this instead:</P>
+        <Pre>
+          <code>yarn dlx htmlgaga</code>
+        </Pre>
       </section>
+      <Footer />
     </div>
   )
 }
