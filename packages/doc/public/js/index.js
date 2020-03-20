2d16c164b527aad85312aa48847490939c9c6f3e
chenxsan <chenxsan@gmail.com>
2020-03-20 09:29:44 +0800

    Build doc

--- packages/doc/public/js/index.js
+++ packages/doc/public/js/index.js
@@ -1,6 +1,7 @@
 /**
  * we write client side javascript here
  */
+import './css.scss'
 window.addEventListener('load', () => {
   import('./log').then(({ default: log }) => {
     log('htmlgaga loaded')
