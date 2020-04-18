972b60e31dfa316d5744be18a3dc90922fb63f28
chenxsan <chenxsan@gmail.com>
2020-04-18 09:44:58 +0800

    Fix tests for windows

--- packages/htmlgaga/src/DevServer/getEntryKeyFromRelativePath.test.ts
+++ packages/htmlgaga/src/DevServer/getEntryKeyFromRelativePath.test.ts
@@ -1,12 +1,13 @@
 import * as path from 'path'
 import fn from './getEntryKeyFromRelativePath'
+const { resolve } = path
 test('should remove file ext from index', () => {
-  expect(fn('/root/pages', '/root/pages/index.js')).toBe(
+  expect(fn(resolve('/root/pages'), resolve('/root/pages/index.js'))).toBe(
     ['index', 'index'].join(path.sep)
   )
 })
 test('should create dir from filename', () => {
-  expect(fn('/root/pages', '/root/pages/detail.js')).toBe(
+  expect(fn(resolve('/root/pages'), resolve('/root/pages/detail.js'))).toBe(
     ['detail', 'index'].join(path.sep)
   )
 })
--- packages/htmlgaga/src/DevServer/index.test.ts
+++ packages/htmlgaga/src/DevServer/index.test.ts
@@ -1,5 +1,6 @@
 import DevServer from './index'
 import { vol } from 'memfs'
+import { resolve } from 'path'
 jest.mock('fs')
 const serverInfo = {
   host: 'localhost',
@@ -11,34 +12,34 @@ describe('DevServer locateSrc method', () => {
   })
   it('should find index.js', () => {
     vol.fromJSON({
-      '/project/a/pages/index.js': '',
+      [resolve('/project/a/pages/index.js')]: '',
     })
-    const server = new DevServer('/project/a/pages', serverInfo)
+    const server = new DevServer(resolve('/project/a/pages'), serverInfo)
     expect(server.locateSrc('/index.html')).toEqual({
       exists: true,
-      src: '/project/a/pages/index.js',
+      src: resolve('/project/a/pages/index.js'),
     })
     expect(server.locateSrc('/')).toEqual({
       exists: true,
-      src: '/project/a/pages/index.js',
+      src: resolve('/project/a/pages/index.js'),
     })
   })
   it('should find dir/index.tsx', () => {
     vol.fromJSON({
-      '/project/a/pages/dir/index.tsx': '',
-      '/project/a/pages/dir/index.js': '',
+      [resolve('/project/a/pages/dir/index.tsx')]: '',
+      [resolve('/project/a/pages/dir/index.js')]: '',
     })
-    const server = new DevServer('/project/a/pages', serverInfo)
+    const server = new DevServer(resolve('/project/a/pages'), serverInfo)
     expect(server.locateSrc('/dir/')).toEqual({
       exists: true,
-      src: '/project/a/pages/dir/index.tsx',
+      src: resolve('/project/a/pages/dir/index.tsx'),
     })
   })
   it('should find nothing', () => {
     vol.fromJSON({
-      '/project/a/pages/index.js': '',
+      [resolve('/project/a/pages/index.js')]: '',
     })
-    const server = new DevServer('/project/a/pages', serverInfo)
+    const server = new DevServer(resolve('/project/a/pages'), serverInfo)
     expect(server.locateSrc('/test.html')).toEqual({
       exists: false,
     })
