290c45aebc4175399a81044fc30dfee0c2e12cc1
chenxsan <chenxsan@gmail.com>
2020-04-18 09:34:40 +0800

    Fix test for windows

--- packages/htmlgaga/src/collectFiles.test.ts
+++ packages/htmlgaga/src/collectFiles.test.ts
@@ -1,5 +1,5 @@
 import collectFiles from './collectFiles'
-import { join } from 'path'
+import { resolve } from 'path'
 import { vol } from 'memfs'
 jest.mock('fs')
 describe('collectFiles', () => {
@@ -8,44 +8,44 @@ describe('collectFiles', () => {
   })
   test('Found one file', async () => {
     vol.fromJSON({
-      [join('/pages/hello.tsx')]: '',
+      [resolve('/pages/hello.tsx')]: '',
     })
-    const pages = await collectFiles(join('/pages'))
-    expect(pages).toEqual([join('/pages/hello.tsx')])
+    const pages = await collectFiles(resolve('/pages'))
+    expect(pages).toEqual([resolve('/pages/hello.tsx')])
   })
   test('Found two files', async () => {
     vol.fromJSON({
-      [join('/pages/hello.tsx')]: '',
-      [join('/pages/hi.tsx')]: '',
+      [resolve('/pages/hello.tsx')]: '',
+      [resolve('/pages/hi.tsx')]: '',
     })
-    const pages = await collectFiles(join('/pages'))
+    const pages = await collectFiles(resolve('/pages'))
     expect(pages.sort()).toEqual([
-      join('/pages/hello.tsx'),
-      join('/pages/hi.tsx'),
+      resolve('/pages/hello.tsx'),
+      resolve('/pages/hi.tsx'),
     ])
   })
   test('Found two files under nested directory', async () => {
     vol.fromJSON({
-      [join('/pages/blog/hi.tsx')]: '',
-      [join('/pages/hello.tsx')]: '',
+      [resolve('/pages/blog/hi.tsx')]: '',
+      [resolve('/pages/hello.tsx')]: '',
     })
-    const pages = await collectFiles(join('/pages'))
+    const pages = await collectFiles(resolve('/pages'))
     expect(pages.sort()).toEqual([
-      join('/pages/blog/hi.tsx'),
-      join('/pages/hello.tsx'),
+      resolve('/pages/blog/hi.tsx'),
+      resolve('/pages/hello.tsx'),
     ])
   })
   test('Found three files under nested directory', async () => {
     vol.fromJSON({
-      [join('/pages/hello.tsx')]: '',
-      [join('/pages/blog/hi.tsx')]: '',
-      [join('/pages/blog/wow.tsx')]: '',
+      [resolve('/pages/hello.tsx')]: '',
+      [resolve('/pages/blog/hi.tsx')]: '',
+      [resolve('/pages/blog/wow.tsx')]: '',
     })
-    const pages = await collectFiles(join('/pages'))
+    const pages = await collectFiles(resolve('/pages'))
     expect(pages.sort()).toEqual([
-      join('/pages/blog/hi.tsx'),
-      join('/pages/blog/wow.tsx'),
-      join('/pages/hello.tsx'),
+      resolve('/pages/blog/hi.tsx'),
+      resolve('/pages/blog/wow.tsx'),
+      resolve('/pages/hello.tsx'),
     ])
   })
 })
