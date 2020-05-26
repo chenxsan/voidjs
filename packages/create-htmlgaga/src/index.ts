267cdd4b617d98d15cf8f14eda8225ae767c88a5
chenxsan <chenxsan@gmail.com>
2020-05-26 20:53:09 +0800

    clean code

--- packages/create-htmlgaga/src/index.ts
+++ packages/create-htmlgaga/src/index.ts
@@ -1,6 +1,4 @@
 import commander from 'commander'
-import prompts from 'prompts'
-import validate from 'validate-npm-package-name'
 import chalk from 'chalk'
 import createApp from './createApp'
 import packageJson from '../package.json'
@@ -23,25 +21,6 @@ const program = new commander.Command(packageJson.name)
   .parse(process.argv)
 
 async function run(): Promise<void> {
-  if (projectName === '') {
-    // ask user to input projectName
-    const response = await prompts({
-      type: 'text',
-      name: 'path',
-      message: 'What is your project name?',
-      initial: 'my-app',
-      validate: (name) => {
-        const validation = validate(name)
-        if (validation.validForNewPackages) {
-          return true
-        }
-        return `Invalid project name: ${
-          validation.errors && validation.errors[0]
-        }`
-      },
-    })
-    projectName = response.path
-  }
   await createApp(projectName, program.template, program.useNpm)
 }
 run()
