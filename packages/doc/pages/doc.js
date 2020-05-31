099ab84b068e3e05961fc0e6f6e41d36948acbc9
Sam Chen <chenxsan@gmail.com>
2020-05-31 23:11:20 +0800

    update doc (#48)

--- packages/doc/pages/doc.js
+++
@@ -1,8 +0,0 @@
-import Docx from '../components/doc.md'
-export default function Doc() {
-  return (
-    <div>
-      <Docx />
-    </div>
-  )
-}
--- packages/doc/pages/index.js
+++ packages/doc/pages/index.js
@@ -1,8 +1,8 @@
+import 'prismjs/themes/prism-okaidia.css'
 import 'css/tailwind.css'
 import logo from 'img/logo.svg'
-import Pre from '../components/Pre'
-import P from '../components/P'
 import Footer from '../components/Footer'
+import Content from '../markdown/home.md'
 import { Helmet } from 'react-helmet'
 export default function Home() {
   return (
@@ -20,32 +20,9 @@ export default function Home() {
         HTMLgaga
       </h1>
       <h2 className="flex justify-center text-base pt-6 px-2 leading-tight font-normal">
-        Manage non-SPA pages with webpack and React.js
+        Manage non-SPA pages with webpack and React
       </h2>
-      <section className="pt-8 px-0 sm:px-2">
-        <h2 className="font-medium text-2xl">System requirements</h2>
-        <ol>
-          <li className="list-disc list-inside">Node.js 10 or later</li>
-          <li className="list-disc list-inside">
-            MacOS, Windows and Linux are supported
-          </li>
-        </ol>
-      </section>
-      <section className="pt-8 px-0 sm:px-2">
-        <h2 className="font-medium text-2xl">Get started</h2>
-        <P>To create a new htmlgaga project, run command:</P>
-        <Pre>
-          <code>npm init htmlgaga</code>
-        </Pre>
-        <P>If you are using yarn 1.x, run this instead:</P>
-        <Pre>
-          <code>yarn create htmlgaga</code>
-        </Pre>
-        <P>If you are using yarn 2.x, run this instead:</P>
-        <Pre>
-          <code>yarn dlx create-htmlgaga</code>
-        </Pre>
-      </section>
+      <Content />
       <Footer />
     </div>
   )
