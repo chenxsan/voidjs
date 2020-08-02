cb62ad08e1a14012f96d3da59063cce1eb7dd8f3
chenxsan <chenxsan@gmail.com>
2020-08-02 20:45:13 +0800

    set max-width for img

--- packages/htmlgaga-styles/pages/index.tsx
+++ packages/htmlgaga-styles/pages/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import '../src/index.scss'
-import Body from '../markdown/body.md'
+import Body from '../markdown/body.mdx'
 export default function Index(): React.ReactNode {
   return (
     <div className="htmlgaga">
@@ -9,9 +9,10 @@ export default function Index(): React.ReactNode {
           textAlign: 'center',
           fontSize: 28,
           padding: '20px 0',
+          textTransform: 'uppercase'
         }}
       >
-        htmlgaga styles for markdown
+        htmlgaga styles
       </header>
       <section>
         <Body />
