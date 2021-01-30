fbb39e210db4cf24d3f3c26de4d64c1d4c005d51
chenxsan <chenxsan@gmail.com>
2021-01-30 10:46:50 +0800

    update text color

--- packages/doc/pages/_app.js
+++ packages/doc/pages/_app.js
@@ -6,7 +6,7 @@ import 'css/tailwind.css'
 // TODO deprecate @void-js/styles as we can customize with mdx
 import '@void-js/styles/lib/index.css'
 import { MDXProvider } from '@mdx-js/react'
-const h2 = (props) => <h2 className="font-serif" {...props} />
+const h2 = (props) => <h2 className="font-serif text-red-600" {...props} />
 export default function App({ Component, pageProps }) {
   return (
     <MDXProvider components={{ h2 }}>
