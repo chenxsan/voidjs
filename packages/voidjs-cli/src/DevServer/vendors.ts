5322fa664539449a887e060717f9fd555d3c9fca
chenxsan <chenxsan@gmail.com>
2021-01-30 15:06:57 +0800

    clean up

--- packages/voidjs-cli/src/DevServer/vendors.ts
+++
@@ -1,6 +0,0 @@
-import type { EntryObject } from './index'
-// share vendors between multiple pages
-const vendors: EntryObject = {
-  'react-vendors': ['react', 'react-dom'],
-}
-export default vendors
