35ba1bb1f40084f0550b5b6f6294f2ce9ad8769c
chenxsan <chenxsan@gmail.com>
2021-01-30 16:08:06 +0800

    add more content

--- packages/doc/markdown/home.md
+++ packages/doc/markdown/home.md
@@ -3,6 +3,12 @@
 - Node.js 12 or later
 - macOS, Windows and Linux are supported
 
+## Use cases
+
+1. Writing html pages without JavaScript.
+2. Writing html pages with a few vanilla JavaScript.
+3. Writing html templates for server side languages other than Node.js, e.g., PHP.
+
 ## Get started
 
 - To create a new voidjs project, run `npm init`:
--- packages/doc/pages/_app.js
+++ packages/doc/pages/_app.js
@@ -6,13 +6,24 @@ import 'css/tailwind-base.css'
 import 'css/tailwind-components.css'
 import 'css/tailwind-utilities.css'
 // TODO deprecate @void-js/styles as we can customize with mdx
-import '@void-js/styles/lib/index.css'
+// import '@void-js/styles/lib/index.css'
 import { MDXProvider } from '@mdx-js/react'
-const h2 = (props) => <h2 className="font-serif text-red-600" {...props} />
-const ul = (props) => <ul className="list-disc list-inside" {...props} />
+const h2 = (props) => (
+  <h2 className="font-serif text-red-600 text-4xl mt-20" {...props} />
+)
+const p = (props) => <p className="text-base" {...props} />
+const ul = (props) => <ul className="list-disc text-base pl-5" {...props} />
+const ol = (props) => <ul className="list-decimal text-base pl-5" {...props} />
+const li = (props) => <li className="pl-2" {...props} />
+const inlineCode = (props) => <code className="bg-gray-200 px-2" {...props} />
+const pre = (props) => <pre className="text-base" {...props} />
 const components = {
   h2,
   ul,
+  ol,
+  inlineCode,
+  p,
+  pre,
 }
 export default function App({ Component, pageProps }) {
   return (
