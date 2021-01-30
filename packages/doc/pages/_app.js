b118f40dfa7308958b10256728d26ed4daf3a237
chenxsan <chenxsan@gmail.com>
2021-01-30 10:44:40 +0800

    add todo

--- packages/doc/pages/_app.js
+++ packages/doc/pages/_app.js
@@ -3,6 +3,7 @@ import 'react-app-polyfill/ie11'
 
 import 'prismjs/themes/prism-okaidia.css'
 import 'css/tailwind.css'
+// TODO deprecate @void-js/styles as we can customize with mdx
 import '@void-js/styles/lib/index.css'
 import { MDXProvider } from '@mdx-js/react'
 const h2 = (props) => <h2 className="font-serif" {...props} />
