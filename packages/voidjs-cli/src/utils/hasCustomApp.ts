5be34e7b690c632a424369ba65f4e4764d6082f3
chenxsan <chenxsan@gmail.com>
2021-01-31 19:06:52 +0800

    add cache

--- packages/voidjs-cli/src/utils/hasCustomApp.ts
+++ packages/voidjs-cli/src/utils/hasCustomApp.ts
@@ -1,11 +1,16 @@
 import fs from 'fs-extra'
 import path from 'path'
+const cache = {}
 export default function hasCustomApp(pagesDir: string): boolean {
-  return (
+  // return if stored in cache
+  if (cache[pagesDir]) return cache[pagesDir]
+
+  // otherwise store the result in cache
+  cache[pagesDir] =
     fs.existsSync(path.join(pagesDir, '_app.tsx')) ||
     fs.existsSync(path.join(pagesDir, '_app.ts')) ||
     fs.existsSync(path.join(pagesDir, '_app.js')) ||
     fs.existsSync(path.join(pagesDir, '_app.jsx')) ||
     fs.existsSync(path.join(pagesDir, '_app.mjs'))
-  )
+  return cache[pagesDir]
 }
