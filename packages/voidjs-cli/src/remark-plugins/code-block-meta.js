05a87b5ca52177b788da1b440421aa4f966f9bf8
chenxsan <chenxsan@gmail.com>
2021-02-14 21:41:42 +0800

    add typing

--- packages/voidjs-cli/src/remark-plugins/code-block-meta.js
+++ packages/voidjs-cli/src/remark-plugins/code-block-meta.js
@@ -1,8 +1,9 @@
 import visit from 'unist-util-visit'
+import type { Visitor } from 'unist-util-visit'
 export default function remarkCodeBlockMeta() {
   return function transformer(tree) {
-    visit(tree, 'code', function visitor(node) {
-      console.log(node)
+    visit(tree, 'code', function visitor(node): Visitor {
+      console.log(node.meta)
       // TODO parse node.meta
     })
   }
