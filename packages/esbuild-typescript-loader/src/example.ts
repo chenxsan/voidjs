2587c6d6b1c5d75505edf5b1d1e97f92df7405a0
chenxsan <chenxsan@gmail.com>
2021-02-21 14:09:50 +0800

    add tests

---
+++ packages/esbuild-typescript-loader/src/example.ts
@@ -0,0 +1,2 @@
+const a: number = 42
+export { a }
---
+++ packages/esbuild-typescript-loader/src/index.test.ts
@@ -0,0 +1,45 @@
+import path from 'path'
+import webpack, { Stats } from 'webpack'
+import { createFsFromVolume, Volume } from 'memfs'
+function webpackCompiler(fixture: string, options = {}): Promise<Stats> {
+  const compiler = webpack({
+    context: __dirname,
+    entry: `./${fixture}`,
+    output: {
+      path: path.resolve(__dirname),
+      filename: 'bundle.js',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          use: [
+            {
+              loader: path.resolve(__dirname, 'index.ts'),
+              options,
+            },
+          ],
+        },
+      ],
+    },
+  })
+  // eslint-disable-next-line
+  // @ts-ignore
+  compiler.outputFileSystem = createFsFromVolume(new Volume())
+  return new Promise((resolve, reject) => {
+    compiler.run((err, stats) => {
+      if (err) reject(err)
+      if (stats?.hasErrors()) reject(stats.toJson().errors)
+      if (typeof stats !== 'undefined') resolve(stats)
+    })
+  })
+}
+describe('esbuild TypeScript Loader', () => {
+  it('should transform TypeScript', async () => {
+    const stats = await webpackCompiler('example.ts')
+    const modules = stats.toJson({ source: true }).modules ?? []
+    const output = modules[0].source
+    expect(output).toContain('const a = 42;')
+    expect(output).toMatchSnapshot()
+  })
+})
